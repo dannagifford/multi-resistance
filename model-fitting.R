449265b936739423e50a3525bab1aeaaa14b522f
Danna Gifford <dannagifford@gmail.com>
2020-09-02 19:45:29 +0100

    updated plots and model fitting code

--- model-fitting.R
+++ model-fitting.R
@@ -1,10 +1,7 @@
-#install.packages(brms)
-#install.packages(tidyverse)
-#install.packages(future)
-
 library(brms)
 library(tidyverse)
 library(future)
+library(nnet)
 
 set.seed(19)
 
@@ -16,24 +13,30 @@ fixtable = function(matrix){
 	as_tibble()
 }
 
+name.brmfile = function(name){
+	paste(run.name, name, "brm_model", sep = "_")
+}
+
 # This number of cores is set for a 12-core system, leaving some cores free for other tasks
 # 3 cores (3 workers) leads to 3 sessions of 3 cores each (in brm calls), meaning 9 cores total!
 # Adjust these values for your system (i.e. don't let mc.cores*3 > your # of cores).
 mc.cores = 4
-plan(multisession, workers = 2)
+plan(multisession, workers = 4)
 
 
-# brms default behaviour is to load a file if it exists, rather than overwrite, and I find this confusing.
-rerun.all.models = F
-if(rerun.all.models == T) system("mv *brm_model.rds old/.")
+# brms default behaviour is to load a file if it exists, rather than overwrite, and I find this confusing, so I clear them out of the way...
+rerun.all.models = T
+if(rerun.all.models == T) {
+	old.models = list.files(pattern="brm_model.rds")
+	if(length(old.models)>0) system(paste("mv", old.models, "old/."))
+}
 
-version = "final"
+# Call this run of models something:
+#run.name = "final"
 # You could also append the time
-#version = format(Sys.time(), format = "%Y-%m-%d-%H:%M:%S")
+run.name = format(Sys.time(), format = "%Y-%m-%d-%H:%M:%S")
 
-make.brmfile = function(name) paste(version, name, "brm_model", sep = "_")
-
-# source("munging.R")
+# source("munging.R") # If not already run
 popns = readRDS("popns.Rds")
 
 popnsAB = popns %>%
@@ -53,42 +56,52 @@ popnsday6 = popnsAB %>%
 	filter(day == 6)
 
 # Model M1: Detection of resistance arising during experimental evolution
+## Here we attempt to fit a categorical model with categorical predictors
 
-priors = c(set_prior ("student_t(7, -5, 2.5)", class = "Intercept"),
+## We choose fairly relaxed priors (though categorical model priors can't
+## be too relaxed. The motivation for these priors is described in the supplement
+
+priors = c(set_prior ("student_t(7, -40, 2.5)", class = "Intercept"),
 		set_prior ("student_t(7, 0, 2.5)", class = "b"))
 
 controls = list(adapt_delta = 0.99, max_treedepth = 15)
 
 # Bernoulli models on double resistance were used to get a feel for the analysis
+# but these results are not given in the paper.
 #brmname = "bernoulli1way"
-#brmfile = make.brmfile(brmname)
+#brmfile = name.brmfile(brmname)
 #ber.1way %<-% {brm (state.double ~ (pmutS.text + antibiotic) + (1|row) + (1|col),
 #	family = bernoulli("logit"), chains = 4, iter = 2000, warmup = 1000,
 #	prior = priors, control = controls, sample_prior = "yes",
 #	data = popnsday6, cores = 4, file = brmfile)}
 
 #brmname = "bernoulli2way"
-#brmfile = make.brmfile(brmname)
+#brmfile = name.brmfile(brmname)
 #ber.2way %<-% {brm (state.double ~ (pmutS.text + antibiotic)^2 + (1|row) + (1|col),
 #	family = bernoulli("logit"), chains = 4, iter = 2000, warmup = 1000,
 #	prior = priors, control = controls, sample_prior = "yes",
 #	data = popnsday6, cores = 4, file = brmfile)}
 
-brmname = "categorical2way"
-brmfile = make.brmfile(brmname)
+brmname = "categorical2way.dir"
+brmfile = name.brmfile(brmname)
 cat.2way %<-% {brm (state.simple ~ (pmutS.text + antibiotic)^2 + (1|row) + (1|col),
 	family = categorical("logit"), chains = 4, iter = 2000, warmup = 1000,
 	prior = priors, control = controls, sample_prior = "yes",
 	data = popnsday6, cores = 4, file = brmfile)}
 
-brmname = "categorical1way"
-brmfile = make.brmfile(brmname)
-cat.1way %<-% {brm (state.simple ~ (pmutS.text + antibiotic) + (1|row) + (1|col),
+brmname = "categorical1way.dir.40"
+brmfile = name.brmfile(brmname)
+#cat.1way %<-% {brm (state.simple ~ (pmutS.text + antibiotic) + (1|row) + (1|col),
+#	family = categorical("logit"), chains = 4, iter = 2000, warmup = 1000,
+#	prior = priors, control = controls, sample_prior = "yes",
+#	data = popnsday6, cores = 4, file = brmfile)}
+
+cat.1way = brm (state.simple ~ (pmutS.text + antibiotic) + (1|row) + (1|col),
 	family = categorical("logit"), chains = 4, iter = 2000, warmup = 1000,
 	prior = priors, control = controls, sample_prior = "yes",
-	data = popnsday6, cores = 4, file = brmfile)}
+	data = popnsday6, cores = 4, file = brmfile)
 
-#WAIC(cat.1way, cat.2way) # WAIC on two objects is depricated in 2.8.0
+#WAIC(cat.1way, cat.2way) # WAIC on two objects is depricated as of brms 2.8.0
 
 cat.1way.waic <- waic(cat.1way)
 cat.2way.waic <- waic(cat.2way)
@@ -106,18 +119,67 @@ loo.compare2 = loo_compare(cat.1way.loo, cat.2way.loo)
 
 # Beware: the models including day can take multiple hours/days to run
 #brmname = "categorical3way_day"
-#brmfile = make.brmfile(brmname)
+#brmfile = name.brmfile(brmname)
 #cat.day.3way %<-% {brm (state.simple ~ (pmutS.text + antibiotic + fday)^3 + (1|row) + (1|col),
 #	family = categorical("logit"), chains = 4, iter = 2000, warmup = 1000,
 #	prior = priors, control = controls, sample_prior = "yes",
 #	data = popnsAB, cores = 4, file = brmfile)}
 
 #brmname = "categorical2way_day"
-#brmfile = make.brmfile(brmname)
+#brmfile = name.brmfile(brmname)
 #cat.day.2way %<-% {brm (state.simple ~ (pmutS.text + antibiotic + fday)^2 + (1|row) + (1|col),
 #	family = categorical("logit"), chains = 4, iter = 2000, warmup = 1000,
 #	prior = priors, control = controls, sample_prior = "yes",
 #	data = popnsAB, cores = 4, file = brmfile)}
+
+
+# Model M1a: Check with nnet multinom guessed priors as requested by reviewer
+
+## Priors set from results of nnet multinom() log-linear model
+## To me, using the data to determine the priors seems kinda circular,
+## but the reviewer suggested that it provides a check for how robust the model
+## is to the priors chosen...
+
+cat.1way.freq = multinom(state.simple ~ (pmutS.text + antibiotic),
+	family = binomial, data = popnsday6)
+
+# Prior means come from model coefficients
+freq.Estimates = summary(cat.1way.freq)$coefficients %>%
+	as.data.frame() %>%
+	rownames_to_column() %>%
+	as_tibble() %>%
+	gather(coef, Estimate, -rowname)
+
+# Prior Sd: estimated Std.err results in inefficient sampling
+# Ultimately set sd to 2.5, as for Model M1 priors.
+freq.Std.err = summary(cat.1way.freq)$standard.error %>%
+	as.data.frame() %>%
+	rownames_to_column() %>%
+	as_tibble() %>%
+	gather(coef, Std.err, -rowname) %>%
+	mutate(Std.err = 2.5)
+
+freq.priors = left_join(freq.Estimates, freq.Std.err) %>%
+	mutate(dpar = sub("^", "mu", rowname)) %>%
+	mutate(coef = gsub("[() ]", "", coef)) %>%
+	mutate(prior = paste0("student_t(7,", Estimate,",", Std.err,")")) %>%
+	mutate(class = ifelse(coef=="Intercept","Intercept","b")) %>%
+	mutate(coef = ifelse(coef=="Intercept","",coef)) %>%
+	select(prior, coef, dpar, class)
+
+checkpriors = set_prior(freq.priors$prior,
+	class = freq.priors$class,
+	dpar = freq.priors$dpar,
+	coef = freq.priors$coef)
+
+# Run the model again with these priors.
+brmname = "nnet.priors"
+brmfile = name.brmfile(brmname)
+cat.1way.check = brm (state.simple ~ (pmutS.text + antibiotic) + (1|row) + (1|col),
+	family = categorical("logit"), chains = 4, iter = 2000, warmup = 1000,
+	prior = checkpriors, control = controls, sample_prior = "yes",
+	data = popnsday6, cores = 4, file = brmfile)
+
 
 
 # Model M2: Fitness of fluctuation-derived strains
@@ -126,7 +188,7 @@ FTcurves.mean = readRDS("FTcurves_mean.Rds")
 # brms doesn't like backticked names, so we rename these first
 # brms 2.8.0 and later uses mvbind() instead of cbind()
 brmname = "FT.model"
-brmfile = make.brmfile(brmname)
+brmfile = name.brmfile(brmname)
 controls = list(adapt_delta = 0.99, max_treedepth = 15)
 priors = c(set_prior ("student_t(7, 10, 2.5)", class = "Intercept"),
 		set_prior ("student_t(7, 0, 2.5)", class = "b"))
@@ -158,7 +220,7 @@ joinedcurves = readRDS("joinedcurves.Rds")
 # brms doesn't like backticked names, so we rename these first
 # brms 2.8.0 and later uses mvbind() instead of cbind()
 brmname = "fitness.model"
-brmfile = make.brmfile(brmname)
+brmfile = name.brmfile(brmname)
 controls = list(adapt_delta = 0.99, max_treedepth = 15)
 priors = c(set_prior ("student_t(7, 10, 2.5)", class = "Intercept"),
 		set_prior ("student_t(7, 0, 2.5)", class = "b"))
@@ -175,7 +237,7 @@ fitness.model = joinedcurves %>%
 		data = ., cores = 4, file = brmfile)
 
 brmname = "fitness.model.intercept"
-brmfile = make.brmfile(brmname)
+brmfile = name.brmfile(brmname)
 controls = list(adapt_delta = 0.99, max_treedepth = 15)
 priors = c(set_prior ("student_t(7, 10, 2.5)", class = "Intercept"))
 
@@ -197,8 +259,8 @@ waic2 <- waic(fitness.model.intercept)
 loo_compare(waic1, waic2)
 
 # Model M4: Compare data and simulation models
-#source("simulations/plots.R") # Run this to generate muller.rds
-muller.sim = readRDS("simulations/muller.rds")
+#source("~/Dropbox (The University of Manchester)/Mutators/simulations/timescale-corrected/dt=0,25-corrected/plottimescale.R")
+muller.sim = readRDS("~/Dropbox (The University of Manchester)/Mutators/simulations/timescale-corrected/dt=0,25-corrected/muller.rds")
 popnsAB.sim = muller.sim %>%
 	group_by(day) %>%
 	filter(time==max(time)&rep<=60) %>%
@@ -221,18 +283,21 @@ priors = c(set_prior ("student_t(7, -5, 2.5)", class = "Intercept"),
 controls = list(adapt_delta = 0.99, max_treedepth = 15)
 
 brmname = "categorical2way_sim"
-brmfile = make.brmfile(brmname)
+brmfile = name.brmfile(brmname)
 cat.2way.sim %<-% {brm (state.simple ~ (pmutS.text + antibiotic)^2, 
 	family = categorical("logit"), chains = 4, iter = 2000, warmup = 1000,
 	prior = priors, control = controls, sample_prior = "yes",
 	data = popnsday6.sim, cores = 4, file = brmfile)}
+#cat.2way.sim = readRDS("2020-07-20-12:27:17_categorical2wayinteraction.rds")
 
 brmname = "categorical1way_sim"
-brmfile = make.brmfile(brmname)
+brmfile = name.brmfile(brmname)
 cat.1way.sim %<-% {brm (state.simple ~ pmutS.text + antibiotic, 
 	family = categorical("logit"), chains = 4, iter = 2000, warmup = 1000,
 	prior = priors, control = controls, sample_prior = "yes",
 	data = popnsday6.sim, cores = 4, file = brmfile)}
+
+#cat.1way.sim = readRDS("2020-07-20-12:27:17_categorical2waymain.rds")
 
 compare_data_sim = left_join(fixtable(fixef(cat.1way)), fixtable(fixef(cat.1way.sim)),
 	by = "parameter", suffix = c(" from experiment"," from simulation")) %>%
@@ -259,5 +324,7 @@ compare_data_sim = left_join(fixtable(fixef(cat.1way)), fixtable(fixef(cat.1way.
 		"low:rifampicin", "low:nalidixic acid", "low:combination",
 		"intermediate:rifampicin", "intermediate:nalidixic acid", "intermediate:combination",
 		"high:rifampicin", "high:nalidixic acid", "high:combination")))
+
+#compare_data_sim$`Agreement of non-zero effect` = (compare_data_sim[5]>0 & compare_data_sim[9]>0 & compare_data_sim[6]>0 & compare_data_sim[10]>0) | (compare_data_sim[5]<0 & compare_data_sim[9]<0 & compare_data_sim[6]<0 & compare_data_sim[10]<0)
 
 saveRDS(compare_data_sim, "compare_data_sim.Rds")
--- plots.R
+++ plots.R
@@ -1,22 +1,36 @@
 ### PLOTTING
-#install.packages(tidyverse)
+#install.packages("tidyverse")
 #install.packages("ggpubr")
 #install.packages("grid")
+#install.packages("egg")
 #install.packages("scales")
-#install.packages("cowplot")
+#install.packages("rflan")
 
 require(tidyverse)
 require(ggpubr)
 require(grid)
+#require(egg)
 require(scales)
-#require(cowplot) 
+require(flan)
+
 
 #source("munging.R") # This code will recreate all Rds files, which is slow, so we load them via readRDS instead after they are created.
+
+tag_facet <- function(p, open = "(", close = ")", tag_pool = letters, x = -Inf, y = Inf, 
+                      hjust = -0.5, vjust = 1.5, fontface = 2, family = "", ...) {
+
+  gb <- ggplot_build(p)
+  lay <- gb$layout$layout
+  tags <- cbind(lay, label = paste0(open, tag_pool[lay$PANEL], close), x = x, y = y)
+  p + geom_text(data = tags, aes_string(x = "x", y = "y", label = "label"), ..., hjust = hjust, 
+                vjust = vjust, fontface = fontface, family = family, inherit.aes = FALSE) 
+}
 
 theme_set(theme_bw())
 theme_update(text = element_text(size = 10),
 	panel.grid.major = element_blank(),
-	panel.grid.minor = element_blank()
+	panel.grid.minor = element_blank(),
+	legend.justification = "top",
 #	strip.background = element_blank()
 )
 
@@ -40,9 +54,7 @@ scientific_10x <- function(values, digits = 1) {
                         paste0("\\1", paste(rep("~", times = longestExponent-1), collapse = "")),
                         "\\1")
   x <- gsub("(e[+|-])[0]", zeroTrimmed, x)
-
   x <- gsub("e", "~x~10^", x)
-
   if(any(grepl("\\^\\-", x))){
     x <- gsub("\\^\\+", "\\^~~", x)
   } else {
@@ -74,9 +86,6 @@ mullert = ggplotGrob(muller)
 mullert[["grobs"]][[18]][["children"]][[2]] = nullGrob()
 muller = as_ggplot(mullert)
 
-ggsave("expevol.pdf", muller, device = "pdf", height = 6, width = 8)
-
-
 #day6 = popns %>%
 #	group_by(volume, day, antibiotic, pmutS.text, pmutS, concentration, state.simple) %>%
 #	summarise(count = n()) %>%
@@ -94,6 +103,8 @@ ggsave("expevol.pdf", muller, device = "pdf", height = 6, width = 8)
 #	filter(day == 6, volume == 1, antibiotic != "no antibiotic") %>%
 #	complete(state.sum, fill = list(count = 0))
 
+pointcolours = bestcolours
+pointcolours[1] = "grey60"
 
 #day6p = day6 %>% ggplot(aes(x = pmutS, y = count)) +
 #	geom_line(aes(group = state.simple, color = state.simple)) +
@@ -106,25 +117,27 @@ ggsave("expevol.pdf", muller, device = "pdf", height = 6, width = 8)
 #	scale_shape_manual(values = c(21,22,23,25,24), name = "Detection of") +
 #	theme(plot.margin = margin(5,14,7,15))
 
+ggsave("expevol.pdf", muller, device = "pdf", height = 6, width = 8)
 
 ### Growth curve plots
 #### Fluctuation test generated strains
-#Not shown in the text, but plots raw growth curves (i.e. OD vs time):
-
-pointcolours = bestcolours
-pointcolours[1] = "grey60"
-
+# Now shown in the supplemental material:
 FTsamples.mean = readRDS("FTsamples_mean.Rds")
 FT.gc.plot = FTsamples.mean %>%
+	ungroup() %>%
 	filter(!grepl("mutator", strain2), time > 1, time <= 30) %>%
+	mutate(concentration = concentration *10) %>%
 	ggplot(aes(x = time, y = OD_mean, color = strain2, linetype = as.factor(id))) +
 	geom_line(aes(group = paste(id, strain2))) +
-	facet_grid(concentration~antibiotic) +
+	facet_grid(antibiotic~concentration) +
 	scale_color_manual(values = pointcolours[-4]) +
 	geom_vline(xintercept = 22, linetype = "11", color = "grey40") +
 	guides(color = guide_legend(title = "Strain"), linetype = F) +
-	scale_y_continuous( sec.axis = sec_axis(~ ., breaks = NULL, labels = NULL, name = "Antibiotic concentration (mg/l)"))
+	scale_x_continuous(name = "Time (h)", sec.axis = sec_axis(~ ., breaks = NULL, labels = NULL, name = "Antibiotic concentration (mg/l)")) +
+	scale_y_continuous(name = "Blanked OD") +
+	theme(legend.position = "bottom")
 
+ggsave("ftgc.pdf", FT.gc.plot, device = "pdf", height = 4, width = 8)
 
 FTcurves.mean = readRDS("FTcurves_mean.Rds")
 FT.auc.plot = FTcurves.mean %>%
@@ -138,11 +151,10 @@ FT.auc.plot = FTcurves.mean %>%
 	scale_color_manual(values = pointcolours[-4], guide = F) +
 	scale_shape_manual(values = c(21,22,23,24), name = "Strain") +
 	scale_x_discrete(labels = unique(FTcurves.mean$concentration*10), breaks = unique(FTcurves.mean$concentration*10), name = "Antibiotic concentration (mg/l)") +
-	scale_y_continuous(name = "Fitness (AUC of OD600)", limits = c(0,12)) +
+	scale_y_continuous(name = "Growth (AUC of OD600)", limits = c(0,12)) +
 	geom_vline(aes(xintercept = ifelse(antibiotic == "combination",5,6)), linetype = "22", color = "grey60") +
 	theme(axis.text.x = element_text(angle = 45, hjust = 1))
 ggsave("ftauc.pdf", FT.auc.plot, device = "pdf", height = 2.5, width = 7)
-
 
 ##### Selection strains
 #Not shown in the text, but plots raw growth curves (i.e. OD vs time):
@@ -173,7 +185,6 @@ ggsave("ftauc.pdf", FT.auc.plot, device = "pdf", height = 2.5, width = 7)
 #	scale_fill_manual(values = c("#ef0358","#ef0358")) +
 #		theme(legend.key = element_rect(color = "white"))
 
-
 # Instead we plot AUC in the presence and absence of antibiotics, coloured by mutator proportion.
 joinedcurves.mean = readRDS("joinedcurves_mean.Rds")
 joined.auc.plot = joinedcurves.mean %>%
@@ -186,8 +197,8 @@ joined.auc.plot = joinedcurves.mean %>%
 		geom_errorbar(aes(ymin = `2_auc_e_mean`-`2_auc_e_sd`, ymax = `2_auc_e_mean`+`2_auc_e_sd`, color = pmutS.text)) +
 		geom_point(size = 2) +
 		geom_abline(slope = 1, intercept = 0, linetype = "22", colour = "grey60") +
-		scale_y_continuous(name = "Fitness in 20 mg/l", limits = c(5,10)) +
-		scale_x_continuous(name = "Fitness in 0 mg/l", limits = c(5,10)) +
+		scale_y_continuous(name = "Growth in 20 mg/l", limits = c(5,10)) +
+		scale_x_continuous(name = "Growth in 0 mg/l", limits = c(5,10)) +
 		scale_colour_manual(values = c("grey60","#f6acc7","#ef0358","#82002f"))+
 		scale_fill_manual(values = c("white","#f6acc7","#ef0358","#82002f"))+
 		scale_shape_manual(values = c(1:4+20)) +
@@ -214,8 +225,6 @@ compare.plot = compare_data_sim %>%
 		guides(shape = FALSE, fill = guide_legend(override.aes = list(shape = c(22,23,23,23,24,24,24))))
 
 ggsave("sim_vs_experiment.pdf", compare.plot, device = "pdf", height = 2, width = 8)
-
-
 dailyOD = readRDS("dailyOD.Rds")
 odp = dailyOD %>%
 	filter(volume == 1) %>%
@@ -238,27 +247,232 @@ grid.newpage()
 grid.draw(odpt)
 dev.off()
 
+# Old NT plot.
+###### OD versus N
+#ODvsNT = readRDS("ODvsNT.Rds")
+#odnp = ODvsNT %>%
+#	ggplot(aes(x=correctedOD, y=NT)) +
+#	geom_point() +
+#	scale_x_continuous(name = " ") +
+#	scale_y_continuous(name = " ", labels = scientific_10x) +
+#	theme(plot.background = element_rect(fill = "transparent", colour = NA)) +
+#	geom_vline(xintercept = 1, color = "grey40", linetype = "22") +
+#	geom_hline(yintercept = 5.71e8, color = "grey40", linetype = "22")
 
-##### OD versus N
-ODvsN = readRDS("ODvsN.Rds")
-odnp = ODvsN %>%
-	ggplot(aes(x=correctedOD, y=NT)) +
-	geom_point() +
-	scale_x_continuous(name = " ") +
-	scale_y_continuous(name = " ", labels = scientific_10x) +
-	theme(plot.background = element_rect(fill = "transparent", colour = NA)) +
-	geom_vline(xintercept = 1, color = "grey40", linetype = "22") +
-	geom_hline(yintercept = 5.71e8, color = "grey40", linetype = "22")
+#odnp_log = odnp +
+#	scale_x_log10(name = "Optical density") +
+#	scale_y_log10(name = "Number of bacteria", labels = trans_format("log10", math_format(10^.x))) +
+#	annotate("text", x = 0.001, y = 1.4*5.71e8, label = scientific_10x(5.71e8, digits = 2), size = 3)
 
-odnp_log = odnp +
-	scale_x_log10(name = "Optical density") +
-	scale_y_log10(name = "Number of bacteria", labels = trans_format("log10", math_format(10^.x))) +
-	annotate("text", x = 0.001, y = 1.4*5.71e8, label = scientific_10x(5.71e8, digits = 2), size = 3)
+#odnp_inset <-
+#  cowplot::ggdraw() +
+#  cowplot::draw_plot(odnp_log) +
+#  cowplot::draw_plot(odnp, x = 0.46, y = 0.1, width = 0.5, height = 0.4)
 
-# We only use cowplot for this one thing so we don't load it into the environment...
-odnp_inset <-
-  cowplot::ggdraw() +
-  cowplot::draw_plot(odnp_log) +
-  cowplot::draw_plot(odnp, x = 0.46, y = 0.1, width = 0.5, height = 0.4)
+#ggsave("ODvsNT.pdf", odnp_inset, width = 3, height = 3)	
 
-ggsave("ODvsN.pdf", odnp_inset, width = 3, height = 3)	
+
+#### New plots to address reviewers' comments
+MHcols = "grey60"
+
+rifcols = rev(c("#1C3929", "#316448", "#469067", "#7DB194", "#B5D2C2", "#eaefec"))
+nalcols = rev(c("#183843", "#235161", "#658590", "#A7B9BF", "#BDCACF","#FFFFFF"))
+doubcols = rev(c("#5F0123", "#A7023E", "#EF0359", "#F34E8A", "#F89ABC", "#FFEFEF"))
+
+odvnt.key = read_csv("odkill/odvnt-key.csv")
+odvnt.count = read_csv("odkill/odvnt.csv")
+
+odvnt.od = tibble(files = list.files(path="odkill/", pattern = ".+OD-NT.+CSV"),
+	data = map(files, ~read_csv(file = paste0("odkill/",.), skip = 2, n_max = 1))) %>%
+	separate(files, into = letters[1:3], sep = "_") %>%
+	mutate(series = gsub(".CSV","",c)) %>%
+	select(series, data) %>%
+	type_convert() %>%
+	arrange(series) %>%
+	unnest() %>%
+	gather(well, OD, `Sample X1`:`Sample X96`) 
+
+odvnt = left_join(left_join(odvnt.key, odvnt.od), odvnt.count)
+
+odvnt.blanks = odvnt %>%
+	filter(is.blank) %>%
+	group_by(series) %>%
+	summarise(blank = mean(OD))
+
+odvnt = odvnt %>%
+	left_join(., odvnt.blanks) %>%
+	mutate(OD.blanked = OD - blank,
+		experiment = "odvnt")
+
+
+kill.key = read_csv("odkill/kill-key.csv")
+kill.count = read_csv("odkill/killcurves.csv")
+
+kill.od = tibble(files = list.files(pattern = ".+Kill.+CSV", path = "odkill/"),
+	data = map(files, ~read_csv(file = paste0("odkill/",.), skip = 2, n_max = 1))) %>%
+	separate(files, into = letters[1:4], sep = "_") %>%
+	mutate(series = gsub("[A-Za-z.]","",d)) %>%
+	select(series, data) %>%
+	type_convert() %>%
+	unnest() %>%
+	gather(well, OD, `Sample X1`:`Sample X96`) %>%
+	arrange(series)
+
+kill = left_join(left_join(kill.key, kill.od), kill.count)
+
+kill.blanks = kill %>%
+	filter(is.blank) %>%
+	group_by(series) %>%
+	summarise(blank = mean(OD))
+
+kill = kill %>%
+	left_join(., kill.blanks) %>%
+	mutate(OD.blanked = OD - blank,
+		experiment = "kill")
+
+
+odvnt_original = read_csv("2018-06-08_ODvsNT.csv") %>%
+	mutate(concentration=0, antibiotic = "MH", experiment = "odvnt_original", strain = "sensitive", series = 0) %>%
+	select(experiment, row=`Well Row`, col=`Well Col`, series, strain, concentration, antibiotic, OD.blanked=correctedOD, NT)
+
+
+alldata = bind_rows(odvnt, kill) %>%
+	mutate(NT = 4*count*10^(-plating)) %>%
+	bind_rows(., odvnt_original) %>%
+	mutate(abconc = paste0(antibiotic, ", ", as.factor(10*concentration))) %>%
+	filter(!is.na(NT)) %>%
+	 select(experiment, row, col, series, strain, abconc, antibiotic, concentration, OD.blanked, NT)
+
+
+abconc = sort(unique(alldata$abconc))
+abconc = c(abconc[7],
+	abconc[c(14,15,17,19,16, 18)],
+	abconc[c(14,15,17,19,16, 18)-6],
+	abconc[c(14,15,17,19,16, 18)-13])
+
+alldata$abconc = factor(alldata$abconc, levels = abconc)
+
+alldata = alldata %>%
+	mutate(strain = recode_factor(strain,
+	sensitive="sensitive",
+	mutS = "mutator",
+	rifampicin = "rifampicin resistant",
+	`nalidixic acid`="nalidixic acid resistant",
+	combination = "double resistant")) %>%
+	mutate(antibiotic = recode_factor(antibiotic,
+	MH="MH",
+	rifampicin = "rifampicin",
+	`nalidixic acid`="nalidixic acid",
+	combination = "combination"))
+
+original = alldata %>%
+	filter(NT>0, experiment=="odvnt_original") %>%
+	ggplot(aes(x = OD.blanked,
+	y = NT,
+	shape = strain,
+	fill = abconc)) +
+	geom_point(size = 2) +
+	scale_shape_manual(values = c(21,25,22:24), name = "Strain") +
+	scale_x_log10() +
+	scale_y_log10(labels = trans_format("log10", math_format(10^.x))) +
+	guides(fill = guide_legend(override.aes=list(shape=21))) +
+	scale_fill_manual(values = c(MHcols, rifcols, nalcols, doubcols), name = "Antibiotic concentration") + 
+	labs(x="Blanked OD", y=" (200μl)") + 
+	facet_wrap(~antibiotic) + 
+	geom_hline(yintercept = 5.71e8, linetype="22") +
+	geom_vline(xintercept = 1, linetype="22")
+
+odp = alldata %>%
+	filter(NT>0, series!=0.01, experiment!="odvnt_original") %>%
+	ggplot(aes(x = OD.blanked,
+	y = NT,
+	shape = strain,
+	fill = abconc)) +
+	geom_point(size = 2) +
+	scale_shape_manual(values = c(21,25,22:24), name = "Strain") +
+	scale_x_log10() +
+	scale_y_log10(labels = trans_format("log10", math_format(10^.x))) +
+	guides(fill = guide_legend(override.aes=list(shape=22, size=5))) +
+	scale_fill_manual(values = c(MHcols, rifcols, nalcols, doubcols), name = "Antibiotic concentration (mg/l)") + 
+	labs(x="Blanked OD", y="Number of bacteria (in 200μl)") + 
+	facet_wrap(~antibiotic, nrow=4) + 
+	theme(legend.key.size = unit(0.8, 'lines')) +
+	theme(plot.margin = unit(c(0,0.5,0,0), "lines"))
+
+killp = alldata %>%
+	filter(concentration>-1, experiment == "kill") %>%
+	ggplot(aes(x = as.factor(series),
+	y = NT,
+	fill = abconc)) +
+	geom_jitter(size = 2, shape = 21, width=0.2) +
+#	scale_shape_manual(values = c(21,25,22:24), name = "Strain") +
+	guides(fill = guide_legend(override.aes=list(shape=23, size=6), ncol=3)) +
+	scale_fill_manual(values = c(MHcols, rifcols[1:6], nalcols[1:6], doubcols[1:6]), name = "Antibiotic concentration (mg/l)") + 
+	labs(x="Time (h)", y="Number of bacteria (in 200μl)") +
+	scale_y_log10(labels = trans_format("log10", math_format(10^.x))) +
+	facet_wrap(~antibiotic, nrow=4) +
+	theme(plot.margin = unit(c(0,0.5,0,0), "lines"))
+
+lmdata = alldata %>%
+	filter(OD.blanked>0.05, NT>0) %>%
+	mutate(antibiotic2 = gsub("combination", "double", antibiotic)) %>%
+	mutate(strain2 = factor(ifelse(grepl("resistant",strain),"resistant","sensitive"),
+		levels = c("sensitive","resistant"))) %>%
+	mutate(grows_in = map2_lgl(strain, antibiotic2,
+		~ grepl(.y, .x)  | .y=="MH")) %>%
+	select(-antibiotic2) %>%
+	filter((experiment == "kill" & series == 24) | experiment == "odvnt" | experiment == "odvnt_original", (grows_in | concentration <=10)) %>%
+	droplevels()
+
+m1 = lm(log10(NT) ~ log10(OD.blanked) * (antibiotic/concentration)*strain, data = lmdata)
+m2 = step(m1)
+summary(m2)
+
+odp_rev = alldata %>%
+	mutate(fitted_vals = 10^predict(m2, newdata = alldata)) %>%
+	filter(NT > 0) %>%
+	ggplot(aes(x = OD.blanked,
+	y = NT,
+	shape = strain,
+	fill = abconc)) +
+	scale_shape_manual(values = c(21,25,22:24), name = "Strain") +
+	scale_x_log10(limits = c(0.01,1)) +
+	scale_y_log10(labels = trans_format("log10", math_format(10^.x))) +
+	guides(fill = guide_legend(override.aes=list(shape=22, size=4), colour=NA)) +
+#	scale_fill_manual(values = c(MHcols, rifcols, nalcols[-3], doubcols[-3]), name = "Antibiotic concentration (mg/l)") + 
+	scale_fill_manual(values = c(MHcols, rifcols, nalcols, doubcols), name = "Antibiotic concentration (mg/l)") + 
+	labs(x="Blanked OD", y="Number of bacteria (in 200μl)") + 
+	facet_wrap(~antibiotic, nrow = 4) + 
+	theme(legend.key.size = unit(0.8, 'lines')) +
+	theme(plot.margin = unit(c(0,0.5,0,0), "lines")) +
+	geom_point(size = 2) +
+	geom_line(aes(x = OD.blanked, y = fitted_vals, colour = abconc), inherit.aes=F) +
+	scale_colour_manual(values = c(MHcols, rifcols, nalcols, doubcols), name = "Antibiotic concentration (mg/l)")
+
+
+joinedODp = ggarrange(odp_rev, killp, ncol = 2, legend.grob = get_legend(odp), legend = "right", labels = "AUTO", font.label = list(face = "plain"), align = "v")
+cairo_pdf("ODvsNT.pdf", width = 6, height = 6)
+joinedODp
+dev.off()
+
+# Reviewer suggestion on p^2
+## Replicates the first 4 days of the experiment (i.e. wild-types could still grow)
+## 10^6 times and counts populations where at least one mutant was observed.
+## Mutation rate mu is product of the two mutation rates to simulate the probability
+## of two independent resistance mutations occurring simultaneously.
+
+set.seed(19)
+flansim = tibble(
+	rep = 1:1e6,
+	pops.mutators = map_dbl(rep,
+		~sum(rflan(n=4*60, mutprob = 3.17e-14, mfn = 5.71e8)$mc > 0)),
+	pops.wt = map_dbl(rep,
+		~sum(rflan(n=4*60, mutprob = 5.0e-18, mfn = 5.71e8)$mc > 0)),
+	prop.mutators = pops.mutators / 60,
+	prop.wt = pops.wt / 60)
+
+mean(flansim$prop.mutators)
+sd(flansim$prop.mutators)
+
+mean(flansim$prop.wt)
+sd(flansim$prop.wt)
